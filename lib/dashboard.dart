bf9bcc79756dfd8939f14d7e084a25ad9732fe82
LiveUser <secured_identity@protonmail.com>
2026-07-30 03:53:09 -0400

    Access token creation, deletion and copy completed

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,6 +1,12 @@
+import 'package:cherry_toast/cherry_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:photonic_drive_server/widgets.dart';
 import 'dart:io';
+import 'package:objective_db/objective_db.dart';
+import 'package:power_plant/power_plant.dart';
+import 'package:confirm_button/confirm_buttons.dart';
+import 'package:flutter/services.dart';
+
 
 class Dashboard extends StatelessWidget {
   const Dashboard({
@@ -66,7 +72,7 @@ class Dashboard extends StatelessWidget {
                 ],
               );
             }else if(snapshot.connectionState == ConnectionState.done){
-              //TODO: Display info like ip, port and allow creating access tokens. Access tokens can be copied or scanned using QR codes.
+              //Display info like ip, port and allow creating access tokens. Access tokens can be copied.
               HttpServer httpServer = snapshot.data as HttpServer;
               return Column(
                 spacing: 10,
@@ -128,12 +134,38 @@ class ServerInfo extends StatelessWidget {
     );
   }
 }
-class TokenManager extends StatelessWidget {
+class TokenManager extends StatefulWidget {
   const TokenManager({
     super.key,
     required this.databaseLocation,
   });
   final String databaseLocation;
+
+  @override
+  State<TokenManager> createState() => _TokenManagerState();
+}
+
+class _TokenManagerState extends State<TokenManager> {
+
+  List<Widget> widgetizeTokens(){
+    List<Widget> widgets = [];
+    Entry entry = Entry(
+      dbPath: widget.databaseLocation,
+    );
+    List<String> accessTokens = [];
+    try{
+      accessTokens = List<String>.from(entry.select().view()["accessTokens"]);
+    }catch(error){
+      accessTokens = [];
+    }
+    for(String accessToken in accessTokens){
+      widgets.add(AccessToken(
+        databaseLocation: widget.databaseLocation,
+        accessToken: accessToken
+      ));
+    }
+    return widgets;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -142,8 +174,20 @@ class TokenManager extends StatelessWidget {
       children: [
         GestureDetector(
           onTap: (){
-            //TODO: Create access token
-
+            //Create access token
+            String newAccessToken = uniqueAlphanumeric(tokenLength: 100);
+            Entry entry = Entry(
+              dbPath: widget.databaseLocation,
+            );
+            entry.select().insert(
+              key: "accessTokens", 
+              value: [
+                newAccessToken,
+              ],
+            );
+            setState(() {
+              
+            });
           },
           child: Container(
             color: Colors.deepPurple,
@@ -169,8 +213,96 @@ class TokenManager extends StatelessWidget {
           ),
         ),
         //TODO: Display access tokens
-        
+        SingleChildScrollView(
+          child: Column(
+            spacing: 10,
+            children: widgetizeTokens(),
+          ),
+        ),
       ],
+    );
+  }
+}
+class AccessToken extends StatefulWidget {
+  const AccessToken({
+    super.key,
+    required this.databaseLocation,
+    required this.accessToken,
+  });
+  final String databaseLocation;
+  final String accessToken;
+
+  @override
+  State<AccessToken> createState() => _AccessTokenState();
+}
+
+class _AccessTokenState extends State<AccessToken> {
+  bool deleted = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return deleted ? SizedBox() : GestureDetector(
+      onTap: (){
+        Clipboard.setData(ClipboardData(
+          text: widget.accessToken
+        ));
+        CherryToast.success(
+          title: Text("Copied access token"),
+        ).show(context);
+      },
+      child: Container(
+        width: double.infinity,
+        color: Colors.deepPurple,
+        padding: EdgeInsets.all(20),
+        child: Row(
+          spacing: 10,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.copy,
+              color: Colors.white,
+            ),
+            Text(
+              widget.accessToken,
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            RadialConfirm(
+              strokeWidth: 4, 
+              secondsToConfirm: 3, 
+              radius: 40,
+              backgroundColor: Colors.deepPurple,
+              valueColor: Colors.white,
+              onConfirmed: (){
+                Entry entry = Entry(
+                  dbPath: widget.databaseLocation,
+                );
+                List<String> accessTokens = [];
+                try{
+                  accessTokens = List<String>.from(entry.select().view()["accessTokens"]);
+                }catch(error){
+                  accessTokens = [];
+                }
+                int index = accessTokens.indexOf(widget.accessToken);
+                if(index != -1){
+                  entry.select().pop(
+                    index: index, 
+                    key: "accessTokens",
+                  );
+                  setState(() {
+                    deleted = true;
+                  });
+                }
+              },
+              child: Icon(
+                Icons.delete,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
